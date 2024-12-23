fa5af068ac8f42fac01bd9bc179b6282f4bcf196
HMIMID <ahmedhmimid177@gmail.com>
2024-12-23 21:25:07 +0100

    feat: update api.js file

--- routes/api.js
+++ routes/api.js
@@ -10,19 +10,25 @@ module.exports = function (app) {
     .post((req, res) => {
       const local = req.body.locale;
       const textRaw = req.body.text;
-      if(!local || !textRaw) {
-        res.json({ error: 'Required field(s) missing' });
-        return;
-      } 
-      if(textRaw === '') {
-        res.json({ error: 'No text to translate' });
-        return;
-      } 
-      if(local !== 'american-to-british' || local !== 'british-to-american') {
-        res.json({ error: 'Invalid value for locale field' });
-        return;
-      } 
-      
+      // if(!local || !textRaw) {
+      //   res.json({ error: 'Required field(s) missing' });
+      //   return;
+      // } 
+      // if(textRaw === '') {
+      //   res.json({ error: 'No text to translate' });
+      //   return;
+      // } 
+      // if(local !== 'american-to-british' || local !== 'british-to-american') {
+      //   res.json({ error: 'Invalid value for locale field' });
+      //   return;
+      // } 
+      if(translator.americanToBritish(textRaw) === "Everything looks good to me!" ||
+         translator.britishToAmerican(textRaw) === "Everything looks good to me!" ) {
+          res.json({
+            text: textRaw,
+            translation: "Everything looks good to me!"
+          });
+         }
       if(local === 'american-to-british') {
         res.json({
           text: textRaw,
