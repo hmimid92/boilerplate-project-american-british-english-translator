fea5cc82e2ebf94e45a70eb8d378569cfe7c6519
HMIMID <ahmedhmimid177@gmail.com>
2024-12-24 16:59:41 +0100

    feat: update api.js file

--- routes/api.js
+++ routes/api.js
@@ -10,14 +10,17 @@ module.exports = function (app) {
     .post((req, res) => {
       const local = req.body.locale;
       const textRaw = req.body.text;
+
+      if(!local || !textRaw) {
+        res.json({ error: 'Required field(s) missing' });
+        return;
+      } 
+
       if(!textRaw) {
         res.json({ error: 'No text to translate' });
         return;
       } 
-      if(!local && !textRaw) {
-        res.json({ error: 'Required field(s) missing' });
-        return;
-      } 
+      
       if(local !== 'american-to-british' || local !== 'british-to-american') {
         res.json({ error: 'Invalid value for locale field' });
         return;
