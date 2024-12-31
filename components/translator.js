0e2539b013671468d427849465797f5377a88560
HMIMID <ahmedhmimid177@gmail.com>
2024-12-31 12:09:04 +0100

    feat: update translator.js and api.js files

--- components/translator.js
+++ components/translator.js
@@ -20,7 +20,7 @@ class Translator {
     const britishArr = Object.keys(Object.fromEntries(mapStructBritish));
     let decide = false;
     britishArr.forEach(e => {
-      if((new RegExp(`${e}`,'gi')).test(text)) {
+      if(text.toLowerCase().includes(e.toLowerCase())) {
        decide = true;
       }     
      });
@@ -46,10 +46,7 @@ class Translator {
     const americanArr = Object.keys(Object.fromEntries(mapStructAmerican));
     let decide = false;
     americanArr.forEach(e => {
-      // if(e.split("").includes(".")) {
-      //   e=e.replace('.','\\.'); 
-      // }
-     if((new RegExp(`${e}`,'gi')).test(text)) {
+     if(text.toLowerCase().includes(e.toLowerCase())) {
       decide = true;
      }     
     });
