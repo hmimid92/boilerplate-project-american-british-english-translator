854e8793656764809bd9b2ed3ab79062cb64bfb1
HMIMID <ahmedhmimid177@gmail.com>
2025-01-01 20:34:57 +0100

    feat: update translator.js and api.js files]

--- components/translator.js
+++ components/translator.js
@@ -39,6 +39,9 @@ class Translator {
     });
     if(!decide) {
       americanArr.forEach(el => {
+        if(el.split('').includes('.')) {
+          el = el.replace('.','\\.');
+        }
         let searched = (new RegExp(`${el}(?=\\s)|[0|1][0-9]:[0-5][0-9]`, 'gi')).exec(text);
         if(searched !== null) {
           if(searched[0] === el) {
